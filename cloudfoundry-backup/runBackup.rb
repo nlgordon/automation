e5b4a1a3a6180d3c2a4c3d134d91882aa50e20fb
Nate Gordon <nlgordon@gmail.com>
2013-01-20 13:50:29 -0600

    Adding a config to determine which cloud foundry servers should be backed up.

--- cloudfoundry-backup/runBackup.rb
+++ cloudfoundry-backup/runBackup.rb
@@ -11,49 +11,60 @@ require "cfoundry"
 require "json"
 require "tunnel-vmc-plugin/tunnel"
 
-file = File.open("/opt/auth/api.cloudfoundry.com.auth")
-parsed = JSON.parse(file.read)
+config = JSON.parse(File.open("/opt/config/cloudfoundry").read)
 
-date = Date.today.to_s
+config["servers"].each do |server, target|
+  puts "Backing up #{server}"
 
-backupDir = "/var/backups/api.cloudfoundry.com/#{date}"
-
-if !File.directory?(backupDir)
-  Dir.mkdir(backupDir)
-end
-
-credentials = {
-  :username => parsed["user"],
-  :password => parsed["password"]
-}
-
-client = CFoundry::Client.new("https://api.cloudfoundry.com", nil)
-#client.trace = true
-
-p "Loggin in"
-client.login(credentials)
-
-p "Getting instances"
-instances = client.service_instances(:depth => 2)
-
-if instances.empty?
-  p "No instances"
-  exit
-end
-
-instances.each do |service|
-  if service.vendor = "mysql"
-    p "#{service.name}"
-    tunnel = CFTunnel.new(client, service)
-    p "Picking Port"
-    port = tunnel.pick_port!("10000")
-    conn_info = tunnel.open!
-    p "Tunnel Open"
-    tunnel.wait_for_start
-    output = "#{backupDir}/backups-#{service.name}.sql"
-    
-    command = "mysqldump --protocol=TCP --host=localhost --port=#{port} --user=#{conn_info["username"]} --password=#{conn_info["password"]} #{conn_info["name"]} > #{output}"
-    p "Backing up #{service.name}"
-    system(command)
+  parsed = JSON.parse(File.open("/opt/auth/#{server}.auth").read)
+  
+  serverBackupDir = "/var/backups/#{server}"
+  
+  if !File.directory?(serverBackupDir)
+    Dir.mkdir(serverBackupDir)
+  end
+  
+  date = Date.today.to_s
+  
+  backupDir = "#{serverBackupDir}/#{date}"
+  
+  if !File.directory?(backupDir)
+    Dir.mkdir(backupDir)
+  end
+  
+  credentials = {
+    :username => parsed["user"],
+    :password => parsed["password"]
+  }
+  
+  client = CFoundry::Client.new(target, nil)
+  #client.trace = true
+  
+  puts "Logging in"
+  client.login(credentials)
+  
+  puts "Getting instances"
+  instances = client.service_instances(:depth => 2)
+  
+  if instances.empty?
+    puts "No instances"
+    exit
+  end
+  
+  instances.each do |service|
+    if service.vendor = "mysql"
+      puts "#{service.name}"
+      tunnel = CFTunnel.new(client, service)
+      puts "Picking Port"
+      port = tunnel.pick_port!("10000")
+      conn_info = tunnel.open!
+      puts "Tunnel Open"
+      tunnel.wait_for_start
+      output = "#{backupDir}/backups-#{service.name}.sql"
+      
+      command = "mysqldump --protocol=TCP --host=localhost --port=#{port} --user=#{conn_info["username"]} --password=#{conn_info["password"]} #{conn_info["name"]} > #{output}"
+      puts "Backing up #{service.name}"
+      system(command)
+    end
   end
 end
