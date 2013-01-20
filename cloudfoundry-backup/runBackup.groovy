dfbc7a37ea7b96302fb0dc6f2644d6ca1cd7102d
root <root@rhel.internal.insanedev.com>
2013-01-19 21:37:32 -0600

    Most of the implementation of connecting and reading what mysql services exist

--- cloudfoundry-backup/runBackup.groovy
+++ cloudfoundry-backup/runBackup.groovy
@@ -1,5 +1,58 @@
 import groovy.json.JsonSlurper
 
 
-JsonSlurper slurper = new JsonSlurper();
-slurper.parseText(new File("/opt/auth/api.cloudfoundry.com.auth").text)
+JsonSlurper slurper = new JsonSlurper()
+def auth = slurper.parseText(new File("/opt/auth/api.cloudfoundry.com.auth").text)
+
+def login = "vmc login --username ${auth.user} --password ${auth.password}"
+
+println "Logging in"
+
+def loginProcess = login.execute()
+
+loginProcess.waitFor()
+
+if (loginProcess.exitValue()) {
+	print loginProcess.err.text
+	exit
+}
+
+def serviceCommand = "vmc services --service mysql -q"
+
+println "Getting services"
+
+def serviceProcess = serviceCommand.execute()
+
+serviceProcess.waitFor()
+
+if (serviceProcess.exitValue()) {
+	print serviceProcess.err.text
+}
+
+def services = []
+
+def serviceLines = serviceProcess.text.eachLine({
+	def matches = it =~ /([^ ]*) .*/
+	services << matches[0][1]
+})
+
+services.each { println it }
+
+Date curDate = new Date().format("YYYY-MM-dd");
+
+File dayDir = new File("/var/backups/api.cloudfoundry.com/${curDate}")
+
+if (!dayDir.exists()) {
+	dayDir.mkdir()
+}
+
+services.each {
+	def backupCommand = "vmc tunnel ${it} mysqldump"
+	def backupProcess = backupCommand.execute()
+	backupProcess.out << "backup-${it}.sql"
+	backupProces.waitFor()
+
+	if (backupProcess.exitValue()) {
+		print backupProcess.err.text
+	}
+}
