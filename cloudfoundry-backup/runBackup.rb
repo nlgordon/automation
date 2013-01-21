2652240c246732617c908c1c4a6aef872970d708
Nate Gordon <nlgordon@gmail.com>
2013-01-21 00:54:37 -0600

    Moving the storage to be backed by an S3 system, I'm using dreamhost.

--- cloudfoundry-backup/runBackup.rb
+++ cloudfoundry-backup/runBackup.rb
@@ -5,32 +5,39 @@ require "yaml"
 require "socket"
 require "net/http"
 require "multi_json"
-require "fileutils"
 
 require "cfoundry"
 require "json"
 require "tunnel-vmc-plugin/tunnel"
 
+require "aws/s3"
+
 config = JSON.parse(File.open("/opt/config/cloudfoundry").read)
 
+s3Config = JSON.parse(File.open("/opt/config/dreamhost-objects").read)
+s3Auth = JSON.parse(File.open("/opt/auth/#{s3Config["server"]}.auth").read)
+
+puts "Connecting to dreamhost - #{s3Config["server"]}"
+
+AWS::S3::Base.establish_connection!(
+        :server            => s3Config["server"],
+        :use_ssl           => true,
+        :access_key_id     => s3Auth["access_key_id"],
+        :secret_access_key => s3Auth["secret_access_key"]
+)
+puts "Connected"
+
+puts "Getting Bucket #{s3Config["backup-bucket"]}"
+bucket = AWS::S3::Bucket.find(s3Config["backup-bucket"])
+
+puts "Bucket Retrieved"
+  
 config["servers"].each do |server, target|
   puts "Backing up #{server}"
 
   parsed = JSON.parse(File.open("/opt/auth/#{server}.auth").read)
   
-  serverBackupDir = "/var/backups/#{server}"
-  
-  if !File.directory?(serverBackupDir)
-    Dir.mkdir(serverBackupDir)
-  end
-  
-  date = Date.today.to_s
-  
-  backupDir = "#{serverBackupDir}/#{date}"
-  
-  if !File.directory?(backupDir)
-    Dir.mkdir(backupDir)
-  end
+  date = DateTime.now.to_s
   
   credentials = {
     :username => parsed["user"],
@@ -60,11 +67,33 @@ config["servers"].each do |server, target|
       conn_info = tunnel.open!
       puts "Tunnel Open"
       tunnel.wait_for_start
-      output = "#{backupDir}/backups-#{service.name}.sql"
       
-      command = "mysqldump --protocol=TCP --host=localhost --port=#{port} --user=#{conn_info["username"]} --password=#{conn_info["password"]} #{conn_info["name"]} > #{output}"
+      backupObject = "#{server}/#{service.name}/#{date}.sql"
+      
+      command = "mysqldump --protocol=TCP --host=localhost --port=#{port} --user=#{conn_info["username"]} --password=#{conn_info["password"]} #{conn_info["name"]}"
       puts "Backing up #{service.name}"
-      system(command)
+      pipe = IO.popen(command, "r")
+      results = pipe.read
+      
+      pipe.close
+      
+      p $?
+      
+      if (!$?.exitstatus)
+        puts "Error backing up"
+        exit
+      end
+      
+      puts "Pushing to Dreamhost as #{backupObject}"
+      AWS::S3::S3Object.store(
+              backupObject,
+              results,
+              s3Config["backup-bucket"],
+              :content_type => 'text/plain'
+      )
+      
+      puts "Done"
     end
   end
 end
+
